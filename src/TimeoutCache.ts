79c814e9e15cb5739da7e8c8204648a3b1682732
Patrick Groß <patrick@pgross.de>
2020-09-09 09:16:35 +0000

    added voice chat exp gain

---
+++ src/TimeoutCache.ts
@@ -0,0 +1,48 @@
+// eslint-disable-next-line max-classes-per-file
+import { Collection } from 'discord.js';
+
+export default class TimeoutCache {
+  private cache = new Collection<string, number>();
+  private expiration: number;
+
+  constructor(expiration: number = 10) {
+    this.expiration = expiration;
+  }
+
+  set(key: string, timestamp?: number): void {
+    this.cache.set(key, timestamp || Date.now());
+    setTimeout(() => this.cache.delete(key), this.expiration * 1000);
+  }
+
+  /**
+   * Returns true if the value is expired.
+   * @param key the key
+   */
+  isExpired(key: string): boolean {
+    const now = Date.now();
+    const timestamp = this.cache.get(key);
+
+    if (timestamp) {
+      const expirationTime = timestamp + this.expiration * 1000;
+
+      if (now < expirationTime) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  timeToExpired(key: string): number | undefined {
+    const now = Date.now();
+    const timestamp = this.cache.get(key);
+
+    if (!timestamp) {
+      return undefined;
+    }
+
+    const expirationTime = timestamp + this.expiration * 1000;
+
+    return (expirationTime - now) / 1000;
+  }
+}
--- src/eventhandler/ExpHandler.ts
+++ src/eventhandler/ExpHandler.ts
@@ -1,6 +1,7 @@
-import { Client, Message } from 'discord.js';
-import { prefix } from '../config.json';
+import { Client, Message, VoiceState, Collection } from 'discord.js';
 import Ranking from '../db/models/Ranking';
+import logger from '../logging';
+import TimeoutCache from '../TimeoutCache';
 import EventHandlerInterface from './EventHandlerInterface';
 
 const MAX_LEVEL = 100;
@@ -9,6 +10,7 @@ const FIRST_LEVEL_EXP = 1000;
 
 // experience gained
 const MESSAGE_EXP = 10;
+const VOICE_PER_M_EXP = 1;
 
 const B = Math.log(MAX_LEVEL_EXP / FIRST_LEVEL_EXP) / (MAX_LEVEL - 1);
 const A = FIRST_LEVEL_EXP / (Math.exp(B) - 1);
@@ -40,34 +42,93 @@ function getLevelForExp(exp: number): number {
 }
 
 export default class ExpHandler extends EventHandlerInterface {
-  messageTimestampCache: { [userId: string]: number } = {};
+  cooldowns = new TimeoutCache(10);
+  inVoiceChatTimestamps = new Collection<string, number>();
 
   constructor(client: Client) {
     super(client);
     this.name = 'exp';
 
-    this.client.on('message', (msg) => {
-      this.execute(msg);
+    this.client.on('message', (message) => {
+      this.onMessage(message);
+    });
+
+    this.client.on('voiceStateUpdate', (oldState, newState) => {
+      this.onVoiceStateUpdate(oldState, newState);
     });
   }
 
-  async execute(message: Message) {
+  async onMessage(message: Message) {
     const userId = message.author.id;
     const guildId = message.guild?.id;
 
-    if (message.author.bot === true || message.content.startsWith(prefix)) {
-      return;
-    }
-    const lastMessageTimestamp =
-      userId in this.messageTimestampCache
-        ? this.messageTimestampCache[userId]
-        : undefined;
-    if (lastMessageTimestamp && Date.now() - lastMessageTimestamp < 10 * 1000) {
+    if (!guildId) {
+      logger.warn('Cannot get guildId in onMessage');
       return;
     }
 
-    this.messageTimestampCache[userId] = message.createdTimestamp;
+    // ignore bots and bot commands
+    if (message.author.bot === true || message.content.match(/^[\\.|,|;|!].*/)) {
+      return;
+    }
 
+    if (!this.cooldowns.isExpired(userId)) {
+      logger.info(
+        `${message.author.username} exp gain is on cooldown for ${this.cooldowns.timeToExpired(
+          userId
+        )} seconds.`
+      );
+
+      return;
+    }
+
+    this.cooldowns.set(userId, message.createdTimestamp);
+
+    await this.giveExp(userId, guildId, MESSAGE_EXP);
+  }
+
+  async onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
+    const userId = newState.member?.user.id;
+    const guildId = newState.guild.id;
+
+    if (!userId) {
+      logger.warn('Cannot get userId in onVoiceStateUpdate');
+      return;
+    }
+
+    // user just switched channels, nothing to do
+    if (oldState.channelID && newState.channelID) {
+      return;
+    }
+
+    // user entered a channel
+    if (!oldState.channelID && newState.channelID) {
+      this.inVoiceChatTimestamps.set(userId, Date.now());
+    }
+
+    // user left a channel
+    if (oldState.channelID && !newState.channelID) {
+      const enteredTimestamp = this.inVoiceChatTimestamps.get(userId);
+      if (!enteredTimestamp) {
+        logger.warn(`Cannot get enteredTimestamp for user ${userId}`);
+        return;
+      }
+      this.inVoiceChatTimestamps.delete(userId);
+
+      const minutesinVc = (Date.now() - enteredTimestamp) / (1000 * 60);
+      const exp = Math.round(minutesinVc * VOICE_PER_M_EXP);
+
+      if (exp > 0) {
+        this.giveExp(userId, guildId, exp);
+
+        logger.info(
+          `${newState.member?.user.username} gained ${exp} exp for being in VC for ${minutesinVc} minutes.`
+        );
+      }
+    }
+  }
+
+  async giveExp(userId: string, guildId: string, exp: number) {
     const result = await Ranking.findOne({
       where: { userId, guildId },
     });
@@ -75,7 +136,7 @@ export default class ExpHandler extends EventHandlerInterface {
     if (!result) {
       await Ranking.create({ userId, guildId });
     } else {
-      result.experience += MESSAGE_EXP;
+      result.experience += exp;
       result.level = getLevelForExp(result.experience);
       await result.save();
     }
